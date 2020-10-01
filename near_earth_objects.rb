f909284212b8333d86c918f89d85f7268dbfc5ff
Arique <arique.aguilar@gmail.com>
2020-09-30 19:14:27 -0500

    Refactored: Added initialize method and created a parse_request method for json

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -7,14 +7,25 @@ Figaro.application = Figaro::Application.new(environment: 'production', path: Fi
 Figaro.load
 
 class NearEarthObjects
-  def self.find_neos_by_date(date)
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
-    asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  def initialize(date)
+    @date = date
+    connect_to_api = Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: @date, api_key: ENV['nasa_api_key']}
+    )
+    @raw_data = connect_to_api.get('/neo/rest/v1/feed')
+  end
+
+  def parse_request
+    JSON.parse(@raw_data.body, symbolize_names: true)[:near_earth_objects][:"#{@date}"]
+  end
+
+  def self.find_neos_by_date(date)
+
+    @neo = NearEarthObjects.new(date)
+
+    parsed_asteroids_data = @neo.parse_request
 
     largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
       asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
